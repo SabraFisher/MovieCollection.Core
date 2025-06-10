756dcca3816c3e321d1f7643dd15d54fd7f251de
Sabra Fisher <labcoatkitty@gmail.com>
2025-06-09 19:35:16 -0700

    moving individual actions to their own methods in helper and working on loading file to array

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -4,10 +4,10 @@ namespace MovieCollection.Core.Models
 {
     public abstract class Movie
     {
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public Genre Genre { get; set; }
         public int ReleaseYear { get; set; }
-        public string Director { get; set; }
+        public string? Director { get; set; }
         public Reviews Reviews { get; set; }
         public Ratings Ratings { get; set; }
         public int Duration { get; set; }
